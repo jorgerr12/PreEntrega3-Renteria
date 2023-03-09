8650be7fa41e5242369dced7b0a3c52185336da5
Jorge Renteria <Jorge_9_90@hotmail.com>
2023-03-08 20:32:05 -0500

    agregar preentrega 2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,160 @@
-const productos = [{
-    nombre: "computadora",
-    precio: 1500
-},
-{
-    nombre: "impresora",
-    precio: 500
-},
-{
-    nombre: "laptop",
-    precio: 2000
-},
-{
-    nombre: "telefono",
-    precio: 1500
-},]
-var comprar = true;
+// variables globales
+let listaProductos =[];
+let listaVentas=[];
+let venta =[];
+// clase de productos.
+class Producto {
+    constructor(data){
+        this.codigo = data.codigo,
+        this.nombre = data.nombre,
+        this.precio = data.precio,
+        this.stock = data.stock
+    }
 
+    vender(){
+        this.stock -= 1;
+    }
+}
+//clase venta
+class Venta {
+    constructor(data){
+        this.cliente = data.cliente,
+        this.productos = data.productos,
+        this.totalPagar = data.totalPagar
+    }
+
+ 
+}
+
+// agregar productos,
+
+
+const createProducto = ()=>{
+    let codigo = (prompt("Ingrese Codigo  de producto")).toLowerCase();
+    let nombre = (prompt("Ingrese Nombre  de producto")).toLowerCase();
+    let precio = Number(prompt("Ingrese precio  de producto"));
+    let stock = Number(prompt("Ingrese stock  de producto"));
+
+
+
+    let data = {
+        codigo,
+        nombre,
+        precio,
+        stock
+    }
+
+    const producto = new Producto(data)
+    listaProductos.push(producto);
+    console.log('Producto Registrado: ',producto);
+    let pregunta =(prompt("desea agregar otro producto? ingrese si o no "));
+    if(pregunta=="si"){
+        createProducto();
+    }
+    
+}
+
+// consultar productos agotados
+
+const getAgotados=()=>{
+    let agotados = listaProductos.filter((p)=>p.stock == 0);
+    return agotados;
+}
+
+// buscar producto por codigo o nombre
+
+const getProducto=(data)=>{
+    let product = data.toLowerCase();
+    let producto = listaProductos.find(p=> p.nombre == product |p.codigo == product)
+    return producto;
+}
+
+const registrarVenta=()=>{
+   
+        let comprar = true;
+        let productos =[];
+        let cliente = (prompt("Ingrese Nombre de cliente")).toLowerCase();
+        // agregar productos a la venta
+        do {
+            
+            let buscado = (prompt("Ingrese nombre o codigo de producto")).toLowerCase();
+            let producto = getProducto(buscado);
+
+            if (producto){
+                productos.push(producto)
+
+                alert(`se agrego producto:${producto.nombre} precio:${producto.precio}`)
+                let pregunta = (prompt("desia agregar otro producto ingrese si o no")).toLowerCase();
+                if(pregunta == "no"){
+                    comprar= false;
+                }
+            }
+            else{
+                alert("producto no existe")
+                let pregunta = (prompt("desea agregar otro producto ingrese si o no")).toLowerCase();
+                if(pregunta == "no"){
+                    comprar= false;
+                }
+            }
+
+        } while (comprar);
+
+        // registar venta
+        let totalventa =0
+        let total = productos.map((producto)=>{
+            
+            totalventa = totalventa + producto.precio;
+            
+        })
+
+        let totalPagar = Number(prompt(`Su monto total a pagar es:${totalventa} ingrese pago`));
+
+        let data = {
+            cliente,
+            productos,
+            totalPagar
+        }
+    
+        let venta = new Venta(data);
+        // Descontar stocks
+        productos.map(producto=>producto.vender())
+        console.log("venta registrada con exito:",venta);
+
+    }
+
+    
+//registrar productos de prueba
+
+    
+const registrarproductos=()=>{
+    const producto1 = new Producto({
+        codigo:"p001",
+        nombre:"laptop",
+        precio:1500,
+        stock:10
+    })
+
+    listaProductos.push(producto1);
+    const producto2 = new Producto({
+        codigo:"p002",
+        nombre:"tablet",
+        precio:800,
+        stock:10
+    })
+    listaProductos.push(producto2);
+    const producto3 = new Producto({
+        codigo:"p003",
+        nombre:"celular",
+        precio:500,
+        stock:1
+    })
+    listaProductos.push(producto3);
+    console.log(listaProductos);
+}
+
+//createProducto();
+
+/* 
 
 
 do {
@@ -51,3 +190,4 @@ do {
     }
 
 } while (comprar);
+ */
