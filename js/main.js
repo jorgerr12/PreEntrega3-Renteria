d11d62160f034f5147046d3e0d369701d245d393
Jorge Renteria <Jorge_9_90@hotmail.com>
2023-03-22 21:44:48 -0500

    add cart, modal, funcionalidad

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 let productos = JSON.parse(localStorage.getItem("Products"));
-let cart = [];
+let cart = JSON.parse(localStorage.getItem("Cart"))||[];
 let count = 0;
 let container = document.getElementById("products");
 const btnBuscar = document.getElementById("btnBuscar");
 const textbuscar = document.getElementById("textbuscar");
+const spanCount = document.getElementById("count");
+const cartBody = document.getElementById("cartBody");
+const textTotal = document.getElementById("textTotal")
 
 const renderProducts = (value) => {
     container.innerHTML = '';
@@ -21,7 +24,7 @@ const renderProducts = (value) => {
                 <spam>${producto.description}
                 </spam>
                 <h2>S/${producto.price}</h2>
-                ${producto.stock > 0 ? '<button onclick="addProducts(${producto.id})" id="btnAdd${producto.id}" type="button" class="btn btn-warning btn-lg" >Agregar al Carrito</button>' : '<button onclick="addProducts(${producto.id})" id="btnAdd${producto.id}" type="button" class="btn btn-warning btn-lg" >Agotado</button>'}
+                ${producto.stock > 0 ? `<button onclick="addProducts(${producto.id})" id="btnAdd${producto.id}" type="button" class="btn btn-warning btn-lg" >Agregar al Carrito</button>` : `<button onclick="addProducts(${producto.id})" id="btnAdd${producto.id}" type="button" class="btn btn-outline-secondary" disabled >Producto Agotado</button>`}
                 
             </article>
             `
@@ -31,8 +34,48 @@ const renderProducts = (value) => {
 
 };
 
+const renderCart = () => {
+    spanCount.innerHTML = cart.length;
+    cartBody.innerHTML='';
+    textTotal.innerHTML="S/0.00";
+    cart.forEach(item => {
+        let product = document.createElement('div');
+        product.className = 'row me-2 p-3 cartItem';
+
+        product.innerHTML = `
+                <div class="col-md-3">
+                    <img src="${item.img}" alt="">
+                </div>
+                <div class="col-md-5">
+                    <span>${item.description}</span>
+                </div>
+                <div class="col-md-2">
+                    <strong style="
+                    font-size: 1.3rem">S/${item.price}</strong>
+                </div>
+                <div class="col-md-2">
+                    <button onClick="deleteProduct(${item.id})" class="btn"><span class="fas fa-trash"></span></button>
+                </div>   
+    `
+    cartBody.appendChild(product);
+    textTotal.innerHTML = `S/${cart.reduce((acc,item)=>acc+item.price,0)}`
+
+    })
+}
+
+const deleteProduct =(id)=>{
+    let item = cart.findIndex(item =>item.id ==id);
+    cart.splice(item,1);
+    localStorage.setItem("Cart",JSON.stringify(cart));
+    renderCart();
+}
+
 const addProducts = (id) => {
-    alert(id);
+    let item = productos.find(item => item.id == id);
+    cart.push(item);
+    localStorage.setItem("Cart",JSON.stringify(cart));
+    renderCart()
+    console.log(cart);
 }
 
 btnBuscar.addEventListener("click", () => {
@@ -41,3 +84,4 @@ btnBuscar.addEventListener("click", () => {
 
 // buscar producto por codigo o nombre
 renderProducts('');
+renderCart();
--- js/products.js
+++ js/products.js
@@ -4,8 +4,8 @@ let Products =[
     {id:3,name:"82KU00SFLM",price:1147.00,description:"Laptop Lenovo IdeaPad 3, 15.6 FHD, Ryzen 3 5300U, 8GB RAM DDR4, 1TB SATA Almacenamiento",category:6,stock:0,img:"assets/img/laptop-ideapad3.jpg"},
     {id:4,name:"Z590 AORUS ULTRA",price:1009.00,description:"Motherboard Gigabyte Z590 Aorus ULTRA, 1200, Z590, DDR4",category:"computo",stock:1,img:"assets/img/motherborad-gigabytez590.jpg"},
     {id:5,name:"TE-2710S",price:529,description:"Monitor Teros TE-2710S, 27 VA, FLAT 75Hz, 1920x1080, Full HD, HDMI",category:"computo",stock:2,img:"assets/img/monitor-teros-te-2710s.jpg"},
-    {id:6,name:"SM-R860NZSALTA",price:840,description:"GALAXY SAMSUNG WATCH 4 40MM",category:"celular",stock:0,img:"assets/img/galaxy-samsumg-wathc4.jpg"},
-    {id:7,name:"U280-Q01FL-BK",price:95,description:"Base de Carga Rápida Inalámbrica Qi Tripp-Lite para Telefonos, Negro.",category:"celular",stock:2,img:"assets/img/base-carga-rapida.jpg"},
+    {id:6,name:"SM-R860NZSALTA",price:840,description:"GALAXY SAMSUNG WATCH 4 40MM",category:"celular",stock:2,img:"assets/img/galaxy-samsumg-wathc4.jpg"},
+    {id:7,name:"U280-Q01FL-BK",price:95,description:"Base de Carga Rápida Inalámbrica Qi Tripp-Lite para Telefonos, Negro.",category:"celular",stock:0,img:"assets/img/base-carga-rapida.jpg"},
     {id:8,name:"CA-9011186-NA",price:715.00,description:"Audífonos gamer con micrófono Corsair Virtuoso Wireless Blanco",category:"celular",stock:3,img:"assets/img/audifonos-gamer-blanco.jpg"},
     {id:9,name:"8KZ-00001",price:320,description:"Microsoft Modern USB-C Speaker",category:"celular",stock:1,img:"assets/img/modern-usb-c-speaker.jpg"},
     
